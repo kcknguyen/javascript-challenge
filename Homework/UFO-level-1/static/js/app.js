43dc5a95f0129ff4530c0751a15bfa77049d436c
kcknguyen <71248778+kcknguyen@users.noreply.github.com>
2021-02-15 21:02:15 -0600

    update

---
+++ Homework/UFO-level-1/static/js/app.js
@@ -0,0 +1,9 @@
+// from data.js
+var tableData = data;
+
+//
+var tbody = d3.select("tbody");
+var button = d3.select("#filter-btn");
+var inputFieldDate = d3.select("#datetime");
+var inputFieldCity = d3.select("#city");
+var columns = ["datetime", "city", "state", "country", "shape", "comments"]
